1e7b690b90273760fe62e5ebc680210b64f54638
Tom Termaat <tom.termaat@hotmail.com>
2025-04-01 00:47:22 +0200

    Fixed Appearance list

--- client/menus/components/apparence-menu.lua
+++ client/menus/components/apparence-menu.lua
@@ -52,7 +52,7 @@ CharacterCreator.Apparence = {
     menu = RageUI.CreateSubMenu(CharacterCreator.Main.menu, "Apparence",
         "Apparence du personnage"),
     render = function()
-        RageUI.List('Cheveux', { 1, 2, 3, 4 }, CharacterCreator.Data.Model.apparence.hair.index, nil, {}, true, {
+        RageUI.List('Cheveux', hairList, CharacterCreator.Data.Model.apparence.hair.index, nil, {}, true, {
             onActive = function()
                 apparenceCategory = 'HAIRS'
                 currentColorPanel = CharacterCreator.Data.Model.apparence.hair.colors
@@ -63,7 +63,7 @@ CharacterCreator.Apparence = {
                 SetPedComponentVariation(PlayerPedId(), 2, index - 1, 0, 2)
             end
         })
-        RageUI.List('Sourcils', { 1, 2, 3, 4 }, CharacterCreator.Data.Model.apparence.eyebrows.index, nil, {}, true, {
+        RageUI.List('Sourcils', eyebrowsList, CharacterCreator.Data.Model.apparence.eyebrows.index, nil, {}, true, {
             onActive = function()
                 apparenceCategory = 'EYEBROWS'
                 currentColorPanel = CharacterCreator.Data.Model.apparence.eyebrows.colors
@@ -71,9 +71,10 @@ CharacterCreator.Apparence = {
             end,
             onListChange = function(index, _)
                 CharacterCreator.Data.Model.apparence.eyebrows.index = index
+                SetPedHeadOverlay(PlayerPedId(), 2, index - 1, 1.0)
             end
         })
-        RageUI.List('Barbes', { 1, 2, 3, 4 }, CharacterCreator.Data.Model.apparence.bear.index, nil, {}, true, {
+        RageUI.List('Barbes', beardList, CharacterCreator.Data.Model.apparence.bear.index, nil, {}, true, {
             onActive = function()
                 apparenceCategory = 'BEARDS'
                 currentColorPanel = CharacterCreator.Data.Model.apparence.bear.colors
@@ -81,10 +82,10 @@ CharacterCreator.Apparence = {
             end,
             onListChange = function(index, _)
                 CharacterCreator.Data.Model.apparence.bear.index = index
-                SetPedHeadOverlay(PlayerPedId(), 1, index - 2, 1.0)
+                SetPedHeadOverlay(PlayerPedId(), 1, index - 1, 1.0)
             end
         })
-        RageUI.List('Yeux', { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, CharacterCreator.Data.Model.apparence.eyes.index, nil, {}, true, {
+        RageUI.List('Yeux', eyeList, CharacterCreator.Data.Model.apparence.eyes.index, nil, {}, true, {
             onActive = function()
                 currentColorPanel = CharacterCreator.Data.Model.apparence.eyes.colors
                 currentPanelColour = RageUI.PanelColour.Eyes
@@ -94,7 +95,7 @@ CharacterCreator.Apparence = {
                 SetPedEyeColor(PlayerPedId(), index - 1, 0, 1)
             end
         })
-        RageUI.List('Taches de rousseurs', { 1, 2, 3, 4 }, CharacterCreator.Data.Model.apparence.frickles.index, nil, {}, true, {
+        RageUI.List('Taches de rousseurs', frecklesList, CharacterCreator.Data.Model.apparence.frickles.index, nil, {}, true, {
             onActive = function()
                 currentColorPanel = CharacterCreator.Data.Model.apparence.frickles.colors
             end,
@@ -103,7 +104,7 @@ CharacterCreator.Apparence = {
                 SetPedHeadOverlay(PlayerPedId(), 9, index - 1, 1.0)
             end
         })
-        RageUI.List('Rides', { 1, 2, 3, 4 }, CharacterCreator.Data.Model.apparence.wrinkles.index, nil, {}, true, {
+        RageUI.List('Rides', ageingList, CharacterCreator.Data.Model.apparence.wrinkles.index, nil, {}, true, {
             onActive = function()
                 currentColorPanel = CharacterCreator.Data.Model.apparence.wrinkles.colors
             end,
@@ -112,7 +113,7 @@ CharacterCreator.Apparence = {
                 SetPedHeadOverlay(PlayerPedId(), 3, index - 1, 1.0)
             end
         })
-        RageUI.List('Coups de soleil', { 1, 2, 3, 4 }, CharacterCreator.Data.Model.apparence.sunDamage.index, nil, {}, true, {
+        RageUI.List('Coups de soleil', sunDamageList, CharacterCreator.Data.Model.apparence.sunDamage.index, nil, {}, true, {
             onActive = function()
                 currentColorPanel = CharacterCreator.Data.Model.apparence.sunDamage.colors
             end,
--- client/utils.lua
+++ client/utils.lua
@@ -1,201 +1,6 @@
 SyncV = exports["sync-v"]:getSyncV()
 
-function Keyboard(TextEntry, MaxStringLenght, Example)
-	AddTextEntry('FMMC_KEY_TIP1', TextEntry)
-	blockinput = true
-	DisplayOnscreenKeyboard(1, "FMMC_KEY_TIP1", "", Example, "", "", "", MaxStringLenght)
-	while UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2 do
-		Wait(0)
-	end
-
-	if UpdateOnscreenKeyboard() ~= 2 then
-		local result = GetOnscreenKeyboardResult()
-		Wait(500)
-		blockinput = false
-		return result
-	else
-		Wait(500)
-		blockinput = false
-		return ""
-	end
-end
-
-function createIntegerList(a, b)
-	local list = {}
-	if a > b then
-		return list
-	end
-	for i = a, b do
-		table.insert(list, i)
-	end
-	return list
-end
-
-function getBirthdate()
-	if CharacterCreator.Data.Identity.informations.birthdate.day == nil
-			or CharacterCreator.Data.Identity.informations.birthdate.month == nil
-			or CharacterCreator.Data.Identity.informations.birthdate.year == nil then
-		return ""
-	else
-		return '~b~'..CharacterCreator.Data.Identity.informations.birthdate.day.."/"
-				..CharacterCreator.Data.Identity.informations.birthdate.month.."/"
-				..CharacterCreator.Data.Identity.informations.birthdate.year
-	end
-end
-
-function LoadAnim(dict)
-    while not HasAnimDictLoaded(dict) do
-      RequestAnimDict(dict)
-      Wait(10)
-    end
-end
-
-function isMalePed()
-    return GetEntityModel(PlayerPedId()) == GetHashKey("mp_m_freemode_01")
-end
-
-function UpdateHeritage()
-    SetPedHeadBlendData(
-        PlayerPedId(),
-        tonumber(moms[CharacterCreator.Data.Model.heredity.momIndex]),
-        tonumber(dads[CharacterCreator.Data.Model.heredity.dadIndex]),
-        nil, 
-        tonumber(moms[CharacterCreator.Data.Model.heredity.momIndex]),
-        tonumber(dads[CharacterCreator.Data.Model.heredity.dadIndex]),
-        nil, 
-        tonumber(CharacterCreator.Data.Model.heredity.heritageBody)/10, 
-        tonumber(CharacterCreator.Data.Model.heredity.heritageSkin)/10,
-        nil, 
-        true
-    )
-end
-
-function UpdateApparence()
-    -- Hair
-    SetPedComponentVariation(PlayerPedId(), 2, CharacterCreator.Data.Model.apparence.hair.index - 1, 0, 2)
-    SetPedHairColor(PlayerPedId(), CharacterCreator.Data.Model.apparence.hair.colors.primary.CurrentIndex - 1, 0)
-
-    -- Bear
-    SetPedHeadOverlay(PlayerPedId(), 1, CharacterCreator.Data.Model.apparence.bear.index - 2, 1.0)
-    SetPedHeadOverlayColor(PlayerPedId(), 1, 1,	CharacterCreator.Data.Model.apparence.bear.colors.primary.CurrentIndex - 1, 0)
-
-    -- Eyes
-    SetPedEyeColor(PlayerPedId(), CharacterCreator.Data.Model.apparence.eyes.index - 1, 0, 1)
-
-    -- Eyebrows
-    SetPedHeadOverlayColor(PlayerPedId(), 2, 1,	CharacterCreator.Data.Model.apparence.eyebrows.colors.primary.CurrentIndex - 1, 0)
-end
-
-function UpdateFeatures()
-    SetPedFaceFeature(PlayerPedId(), 0, CharacterCreator.Data.Model.features.nose.x)
-    SetPedFaceFeature(PlayerPedId(), 1, CharacterCreator.Data.Model.features.nose.y)
-    SetPedFaceFeature(PlayerPedId(), 2, CharacterCreator.Data.Model.features.noseProfile.x)
-    SetPedFaceFeature(PlayerPedId(), 3, CharacterCreator.Data.Model.features.noseProfile.y)
-    SetPedFaceFeature(PlayerPedId(), 4, CharacterCreator.Data.Model.features.nosePeak.x)
-    SetPedFaceFeature(PlayerPedId(), 5, CharacterCreator.Data.Model.features.nosePeak.y)
-    SetPedFaceFeature(PlayerPedId(), 7, CharacterCreator.Data.Model.features.eyebrows.x)
-    SetPedFaceFeature(PlayerPedId(), 6, CharacterCreator.Data.Model.features.eyebrows.y)
-    SetPedFaceFeature(PlayerPedId(), 8, CharacterCreator.Data.Model.features.cheekbones.x)
-    SetPedFaceFeature(PlayerPedId(), 9, CharacterCreator.Data.Model.features.cheekbones.y)
-    SetPedFaceFeature(PlayerPedId(), 10, CharacterCreator.Data.Model.features.cheeks.x) 
-    SetPedFaceFeature(PlayerPedId(), 11, CharacterCreator.Data.Model.features.eyes.x)
-    SetPedFaceFeature(PlayerPedId(), 12, CharacterCreator.Data.Model.features.Lips.x)
-    SetPedFaceFeature(PlayerPedId(), 13, CharacterCreator.Data.Model.features.jaws.x)
-    SetPedFaceFeature(PlayerPedId(), 14, CharacterCreator.Data.Model.features.jaws.y)
-    SetPedFaceFeature(PlayerPedId(), 15, CharacterCreator.Data.Model.features.chin.x)
-    SetPedFaceFeature(PlayerPedId(), 16, CharacterCreator.Data.Model.features.chin.y)
-    SetPedFaceFeature(PlayerPedId(), 17, CharacterCreator.Data.Model.features.chinSize.x)
-    SetPedFaceFeature(PlayerPedId(), 18, CharacterCreator.Data.Model.features.chinSize.y)
-    SetPedFaceFeature(PlayerPedId(), 19, CharacterCreator.Data.Model.features.neckThickness.x)
-end
-
-function UpdateOutfit()
-    for k, v in pairs(CharacterCreator.Data.Outfit.clothes) do
-        SetPedComponentVariation(PlayerPedId(), k, v[1], v[2], 2)
-    end
-end
-
-function RefreshModel()
-	Citizen.CreateThread(function()
-        local model = models[CharacterCreator.Data.Model.gender]
-
-		while not HasModelLoaded(model) do
-            RequestModel(model)
-            Wait(0)
-        end
-
-        SetPlayerModel(PlayerId(), model)
-
-        UpdateHeritage()
-        UpdateApparence()
-        UpdateFeatures()
-        UpdateOutfit()
-	end)
-end
-
-function AnimCam()
-    DoScreenFadeOut(1000)
-    Citizen.Wait(4000)
-    DestroyAllCams(true)
-	initpos = GetEntityCoords(PlayerPedId())
-	
-    -- Refresh model
-    RefreshModel()
-
-	local cam2 = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", Camera['body'].x, Camera['body'].y, Camera['body'].z, 0.00, 0.00, 0.00, Camera['body'].fov, false, 0)
-    SetCamActive(cam2, true)
-    RenderScriptCams(true, false, 2000, true, true) 
-    
-    Citizen.Wait(500)
-    
-    DoScreenFadeIn(2000)
-    SetEntityCoords(PlayerPedId(), 405.59, -997.18, -99.00, 0.0, 0.0, 0.0, true)
-    SetEntityHeading(PlayerPedId(), 90.00)
-   
-    -- TriggerEvent('skinchanger:loadSkin', {sex = 0})
-    Citizen.Wait(500)
-    
-    local cam3 = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", 402.99, -998.02, -99.00, 0.00, 0.00, 0.00, 50.00, false, 0)
-    PointCamAtCoord(cam3, 402.99, -998.02, -99.00)
-    SetCamActiveWithInterp(cam2, cam3, 5000, true, true)
-    LoadAnim("mp_character_creation@customise@male_a")
-    TaskPlayAnim(PlayerPedId(), "mp_character_creation@customise@male_a", "intro", 1.0, 1.0, 4050, 0, 1, 0, 0, 0)
-	
-    Citizen.Wait(4500)
-
-    local coords = GetEntityCoords(PlayerPedId())
-    if GetDistanceBetweenCoords(coords, 402.89, -996.87, -99.0, true) > 0.5 then
-    	SetEntityCoords(PlayerPedId(), 402.89, -996.87, -99.0, 0.0, 0.0, 0.0, true)
-    	SetEntityHeading(PlayerPedId(), 173.97)
-    end
-
-    FreezeEntityPosition(PlayerPedId(), true)
-end
-
-function EndCharCreator()
-    CloseMenu()
-	FreezeEntityPosition(PlayerPedId(), false)
-
-	if CharacterCreator.Data.Model.gender == 1 then
-		LoadAnim("mp_character_creation@lineup@male_b")
-		TaskPlayAnim(PlayerPedId(), "mp_character_creation@lineup@male_b", "outro", 0.225, 1.0, 6000, 0, 1, 0, 0, 0)
-	else
-		LoadAnim("mp_character_creation@lineup@female_a")
-		TaskPlayAnim(PlayerPedId(), "mp_character_creation@lineup@female_a", "outro", 0.225, 1.0, 6000, 0, 1, 0, 0, 0)
-	end
-	Citizen.Wait(5000)
-	DoScreenFadeOut(1000)
-    enable = false
-    RenderScriptCams(false,  false,  0,  true,  true)
-	EnableAllControlActions(0)
-    FreezeEntityPosition(PlayerPedId(), false)
-	DisplayRadar(true)
-	Citizen.Wait(5000)
-	DoScreenFadeIn(1000)
-    Citizen.Wait(1000)
-    ClearFocus()
-end
-
+local initpos
 cam, cam2, cam3, camSkin, isCameraActive = nil, nil, nil, nil, nil
 
 models = { 'mp_m_freemode_01', 'mp_f_freemode_01' }
@@ -374,6 +179,11 @@ foutfits = {
 	}
 }
 
+local Camera = {
+	face = {x = 402.74, y = -1000.72, z = -98.45, fov = 10.00},
+	body = {x = 402.92, y = -1000.72, z = -99.01, fov = 30.00},
+}
+
 nationalityIndex = 1
 heightIndex = 1
 weightIndex = 1
@@ -385,13 +195,217 @@ nationalityList = {
 	"Portugais", "Argentin", "Néerlandais", "Suisse", "Suédois"
 }
 
-heightList = createIntegerList(150, 210)
-weightList = createIntegerList(40, 180)
+function createIntegerList(a, b)
+	local list = {}
+	if a > b then
+		return list
+	end
+	for i = a, b do
+		table.insert(list, i)
+	end
+	return list
+end
 
 dayIndex = 1
 monthIndex = 1
 yearIndex = 1
 
+heightList = createIntegerList(150, 210)
+weightList = createIntegerList(40, 180)
+
 dayList = createIntegerList(1, 31)
 monthList = createIntegerList(1, 12)
 yearList = createIntegerList(1950, 2025)
+
+hairList = createIntegerList(1, 82)
+eyeList = createIntegerList(1, 30)
+beardList = createIntegerList(1, 29)
+eyebrowsList = createIntegerList(1, 34)
+frecklesList = createIntegerList(1, 18)
+ageingList = createIntegerList(1, 15)
+sunDamageList = createIntegerList(1, 11)
+
+function Keyboard(TextEntry, MaxStringLenght, Example)
+	AddTextEntry('FMMC_KEY_TIP1', TextEntry)
+	blockinput = true
+	DisplayOnscreenKeyboard(1, "FMMC_KEY_TIP1", "", Example, "", "", "", MaxStringLenght)
+	while UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2 do
+		Wait(0)
+	end
+
+	if UpdateOnscreenKeyboard() ~= 2 then
+		local result = GetOnscreenKeyboardResult()
+		Wait(500)
+		blockinput = false
+		return result
+	else
+		Wait(500)
+		blockinput = false
+		return ""
+	end
+end
+
+function getBirthdate()
+	if CharacterCreator.Data.Identity.informations.birthdate.day == nil
+			or CharacterCreator.Data.Identity.informations.birthdate.month == nil
+			or CharacterCreator.Data.Identity.informations.birthdate.year == nil then
+		return ""
+	else
+		return '~b~'..CharacterCreator.Data.Identity.informations.birthdate.day.."/"
+				..CharacterCreator.Data.Identity.informations.birthdate.month.."/"
+				..CharacterCreator.Data.Identity.informations.birthdate.year
+	end
+end
+
+function LoadAnim(dict)
+    while not HasAnimDictLoaded(dict) do
+      RequestAnimDict(dict)
+      Wait(10)
+    end
+end
+
+function isMalePed()
+    return GetEntityModel(PlayerPedId()) == GetHashKey("mp_m_freemode_01")
+end
+
+function UpdateHeritage()
+    SetPedHeadBlendData(
+        PlayerPedId(),
+        tonumber(moms[CharacterCreator.Data.Model.heredity.momIndex]),
+        tonumber(dads[CharacterCreator.Data.Model.heredity.dadIndex]),
+        nil, 
+        tonumber(moms[CharacterCreator.Data.Model.heredity.momIndex]),
+        tonumber(dads[CharacterCreator.Data.Model.heredity.dadIndex]),
+        nil, 
+        tonumber(CharacterCreator.Data.Model.heredity.heritageBody)/10, 
+        tonumber(CharacterCreator.Data.Model.heredity.heritageSkin)/10,
+        nil, 
+        true
+    )
+end
+
+function UpdateApparence()
+    -- Hair
+    SetPedComponentVariation(PlayerPedId(), 2, CharacterCreator.Data.Model.apparence.hair.index - 1, 0, 2)
+    SetPedHairColor(PlayerPedId(), CharacterCreator.Data.Model.apparence.hair.colors.primary.CurrentIndex - 1, 0)
+
+    -- Bear
+    SetPedHeadOverlay(PlayerPedId(), 1, CharacterCreator.Data.Model.apparence.bear.index - 2, 1.0)
+    SetPedHeadOverlayColor(PlayerPedId(), 1, 1,	CharacterCreator.Data.Model.apparence.bear.colors.primary.CurrentIndex - 1, 0)
+
+    -- Eyes
+    SetPedEyeColor(PlayerPedId(), CharacterCreator.Data.Model.apparence.eyes.index - 1, 0, 1)
+
+    -- Eyebrows
+    SetPedHeadOverlayColor(PlayerPedId(), 2, 1,	CharacterCreator.Data.Model.apparence.eyebrows.colors.primary.CurrentIndex - 1, 0)
+end
+
+function UpdateFeatures()
+    SetPedFaceFeature(PlayerPedId(), 0, CharacterCreator.Data.Model.features.nose.x)
+    SetPedFaceFeature(PlayerPedId(), 1, CharacterCreator.Data.Model.features.nose.y)
+    SetPedFaceFeature(PlayerPedId(), 2, CharacterCreator.Data.Model.features.noseProfile.x)
+    SetPedFaceFeature(PlayerPedId(), 3, CharacterCreator.Data.Model.features.noseProfile.y)
+    SetPedFaceFeature(PlayerPedId(), 4, CharacterCreator.Data.Model.features.nosePeak.x)
+    SetPedFaceFeature(PlayerPedId(), 5, CharacterCreator.Data.Model.features.nosePeak.y)
+    SetPedFaceFeature(PlayerPedId(), 7, CharacterCreator.Data.Model.features.eyebrows.x)
+    SetPedFaceFeature(PlayerPedId(), 6, CharacterCreator.Data.Model.features.eyebrows.y)
+    SetPedFaceFeature(PlayerPedId(), 8, CharacterCreator.Data.Model.features.cheekbones.x)
+    SetPedFaceFeature(PlayerPedId(), 9, CharacterCreator.Data.Model.features.cheekbones.y)
+    SetPedFaceFeature(PlayerPedId(), 10, CharacterCreator.Data.Model.features.cheeks.x) 
+    SetPedFaceFeature(PlayerPedId(), 11, CharacterCreator.Data.Model.features.eyes.x)
+    SetPedFaceFeature(PlayerPedId(), 12, CharacterCreator.Data.Model.features.Lips.x)
+    SetPedFaceFeature(PlayerPedId(), 13, CharacterCreator.Data.Model.features.jaws.x)
+    SetPedFaceFeature(PlayerPedId(), 14, CharacterCreator.Data.Model.features.jaws.y)
+    SetPedFaceFeature(PlayerPedId(), 15, CharacterCreator.Data.Model.features.chin.x)
+    SetPedFaceFeature(PlayerPedId(), 16, CharacterCreator.Data.Model.features.chin.y)
+    SetPedFaceFeature(PlayerPedId(), 17, CharacterCreator.Data.Model.features.chinSize.x)
+    SetPedFaceFeature(PlayerPedId(), 18, CharacterCreator.Data.Model.features.chinSize.y)
+    SetPedFaceFeature(PlayerPedId(), 19, CharacterCreator.Data.Model.features.neckThickness.x)
+end
+
+function UpdateOutfit()
+    for k, v in pairs(CharacterCreator.Data.Outfit.clothes) do
+        SetPedComponentVariation(PlayerPedId(), k, v[1], v[2], 2)
+    end
+end
+
+function RefreshModel()
+	Citizen.CreateThread(function()
+        local model = models[CharacterCreator.Data.Model.gender]
+
+		while not HasModelLoaded(model) do
+            RequestModel(model)
+            Wait(0)
+        end
+
+        SetPlayerModel(PlayerId(), model)
+
+        UpdateHeritage()
+        UpdateApparence()
+        UpdateFeatures()
+        UpdateOutfit()
+	end)
+end
+
+function AnimCam()
+    DoScreenFadeOut(1000)
+    Citizen.Wait(4000)
+    DestroyAllCams(true)
+	initpos = GetEntityCoords(PlayerPedId())
+	
+    -- Refresh model
+    RefreshModel()
+
+	local cam2 = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", Camera['body'].x, Camera['body'].y, Camera['body'].z, 0.00, 0.00, 0.00, Camera['body'].fov, false, 0)
+    SetCamActive(cam2, true)
+    RenderScriptCams(true, false, 2000, true, true) 
+    
+    Citizen.Wait(500)
+    
+    DoScreenFadeIn(2000)
+    SetEntityCoords(PlayerPedId(), 405.59, -997.18, -99.00, 0.0, 0.0, 0.0, true)
+    SetEntityHeading(PlayerPedId(), 90.00)
+   
+    -- TriggerEvent('skinchanger:loadSkin', {sex = 0})
+    Citizen.Wait(500)
+    
+    local cam3 = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", 402.99, -998.02, -99.00, 0.00, 0.00, 0.00, 50.00, false, 0)
+    PointCamAtCoord(cam3, 402.99, -998.02, -99.00)
+    SetCamActiveWithInterp(cam2, cam3, 5000, true, true)
+    LoadAnim("mp_character_creation@customise@male_a")
+    TaskPlayAnim(PlayerPedId(), "mp_character_creation@customise@male_a", "intro", 1.0, 1.0, 4050, 0, 1, 0, 0, 0)
+	
+    Citizen.Wait(4500)
+
+    local coords = GetEntityCoords(PlayerPedId())
+    if GetDistanceBetweenCoords(coords, 402.89, -996.87, -99.0, true) > 0.5 then
+    	SetEntityCoords(PlayerPedId(), 402.89, -996.87, -99.0, 0.0, 0.0, 0.0, true)
+    	SetEntityHeading(PlayerPedId(), 173.97)
+    end
+
+    FreezeEntityPosition(PlayerPedId(), true)
+end
+
+function EndCharCreator()
+    CloseMenu()
+	FreezeEntityPosition(PlayerPedId(), false)
+
+	if CharacterCreator.Data.Model.gender == 1 then
+		LoadAnim("mp_character_creation@lineup@male_b")
+		TaskPlayAnim(PlayerPedId(), "mp_character_creation@lineup@male_b", "outro", 0.225, 1.0, 6000, 0, 1, 0, 0, 0)
+	else
+		LoadAnim("mp_character_creation@lineup@female_a")
+		TaskPlayAnim(PlayerPedId(), "mp_character_creation@lineup@female_a", "outro", 0.225, 1.0, 6000, 0, 1, 0, 0, 0)
+	end
+	Citizen.Wait(5000)
+	DoScreenFadeOut(1000)
+    enable = false
+    RenderScriptCams(false,  false,  0,  true,  true)
+	EnableAllControlActions(0)
+    FreezeEntityPosition(PlayerPedId(), false)
+	DisplayRadar(true)
+	Citizen.Wait(5000)
+	DoScreenFadeIn(1000)
+    Citizen.Wait(1000)
+    ClearFocus()
+end
